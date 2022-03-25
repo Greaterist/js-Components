32a754af075e57d1aa805511a0a6c20df987ffa6
Ivan Taktaev <32547753+Greaterist@users.noreply.github.com>
2022-03-25 11:57:52 +0300

    Update stringOperations.js

--- json/stringOperations.js
+++ json/stringOperations.js
@@ -3,5 +3,10 @@ export function jsonFormattedToLine(input){
 }
 
 export function jsonLineToFormatted(input){
-  return JSON.stringify(JSON.parse(input), null, "\t")
+  try {
+        let output = JSON.parse(input);
+    } catch(e) {
+        alert(e);
+    }
+  return JSON.stringify(output, null, "\t")
 }
